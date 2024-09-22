5516b3674610ad83e36afb2bc79451f4b8207481
Charles <charlesgunawan32@gmail.com>
2024-09-22 17:21:12 +0700

    add imagekit

--- routes/mitra.js
+++ routes/mitra.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const mitraControllers = require('../controllers/mitra')
 const middleware = require('../middleware/verifyToken')
-const multer = require('../middleware/multer')
+const {upload} = require('../middleware/multer')
 
-// router.get('/', mitraControllers.getAllMitra)
-// router.post('/', middleware.isAdmin, multer.image("logo-mitra").single("image"), mitraControllers.createMitra)
-// router.get('/:id', mitraControllers.getMitraById)
-// router.put('/:id', middleware.isAdmin, multer.image("logo-mitra").single("image"), mitraControllers.updateMitra)
-// router.delete('/:id', middleware.isAdmin, mitraControllers.deleteMitra)
+router.get('/', mitraControllers.getAllMitra)
+router.post('/', middleware.isAdmin, upload.single("image"), mitraControllers.createMitra)
+router.get('/:id', mitraControllers.getMitraById)
+router.put('/:id', middleware.isAdmin, upload.single("image"), mitraControllers.updateMitra)
+router.delete('/:id', middleware.isAdmin, mitraControllers.deleteMitra)
 
 module.exports = router;
